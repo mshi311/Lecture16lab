9bf7dc5e6124d29259655839077101f4a393f02b
cbuwana <colley.windya@gmail.com>
2024-02-27 16:25:31 -0600

    Editing a line
    I'm adding words to the two lines that we already have in the repository.

--- mycode.R
+++ mycode.R
@@ -1,5 +1,5 @@
 # My R code
-print("This is my R code.")
+print("This is my R code. And I am editing a line here.")
 
-print("I added more to my R code.")
+print("I added more to my R code. I add this line too")
 
